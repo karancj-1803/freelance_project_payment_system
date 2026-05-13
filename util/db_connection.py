c242b1531b9b1c325bf3b627d8171871274d5ceb
Karan C J <chandrabosekaran@gmail.com>
2026-05-13 09:45:45 +0530

    Written code for db connection util file.

---
+++ util/db_connection.py
@@ -0,0 +1,41 @@
+import pyodbc as db
+
+class DBConnection:
+    con = None
+    
+    @staticmethod
+    def get_connection():
+        
+        if DBConnection.con == None:
+            try:
+                connection_string = """
+                                    DRIVER={ODBC Driver 17 for SQL Server};
+                                    SERVER=localhost\\SQLEXPRESS;
+                                    DATABASE=appdb;
+                                    Trusted_Connection=yes;
+                                    """
+                DBConnection.con = db.connect(connection_string)
+                print("Database connection successful.")
+            
+            except:
+                print("Database connection unsuccessful.")
+            
+        return DBConnection.con
+    
+    @staticmethod
+    def close_connection():
+        if DBConnection.con is not None:
+            print("Connection closed successfully.")
+            return DBConnection.con.close()
+        else:
+            print("Connection already closed.")
+            
+dbc = DBConnection()
+
+con = DBConnection.get_connection()
+
+print(con)
+
+con = DBConnection.close_connection()
+
+print(con)
